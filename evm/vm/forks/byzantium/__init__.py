68cb29513d947542c3257eb649a8c760caafd6a3
Piper Merriam <pipermerriam@gmail.com>
2017-10-20 11:41:16 -0600

    dirty

--- evm/vm/forks/byzantium/__init__.py
+++ evm/vm/forks/byzantium/__init__.py
@@ -1,10 +1,13 @@
 from ..spurious_dragon import SpuriousDragonVM
 
 from .opcodes import BYZANTIUM_OPCODES
+from .blocks import ByzantiumBlock
 
 
 ByzantiumVM = SpuriousDragonVM.configure(
     name='ByzantiumVM',
     # opcodes
     opcodes=BYZANTIUM_OPCODES,
+    # RLP
+    _block_class=ByzantiumBlock,
 )
