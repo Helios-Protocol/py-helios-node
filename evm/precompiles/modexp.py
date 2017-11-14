d64279439e187578f48324c7ffe4839d1e10d77b
Piper Merriam <pipermerriam@gmail.com>
2017-11-13 20:00:58 -0700

    fix modexp

--- evm/precompiles/modexp.py
+++ evm/precompiles/modexp.py
@@ -12,16 +12,15 @@ from evm.utils.padding import (
 )
 
 
-def _compute_adjusted_exponent_length(exponent_bytes):
-    exponent_length = len(exponent_bytes)
-    exponent = big_endian_to_int(exponent_bytes)
+def _compute_adjusted_exponent_length(exponent_length, first_32_exponent_bytes):
+    exponent = big_endian_to_int(first_32_exponent_bytes)
 
     if exponent_length <= 32 and exponent == 0:
         return 0
     elif exponent_length <= 32:
         return get_highest_bit_index(exponent)
     else:
-        first_32_bytes_as_int = big_endian_to_int(exponent_bytes[:32])
+        first_32_bytes_as_int = big_endian_to_int(first_32_exponent_bytes)
         return (
             8 * (exponent_length - 32) +
             get_highest_bit_index(first_32_bytes_as_int)
@@ -40,8 +39,31 @@ def _compute_complexity(length):
         return 2 ** 2 // 16 + 480 * length - 199680
 
 
-def _compute_modexp_gas_fee(exponent_bytes, base_length, modulus_length):
-    adjusted_exponent_length = _compute_adjusted_exponent_length(exponent_bytes)
+def _extract_lengths(data):
+    # extract argument lengths
+    base_length_bytes = pad32r(data[:32])
+    base_length = big_endian_to_int(base_length_bytes)
+
+    exponent_length_bytes = pad32r(data[32:64])
+    exponent_length = big_endian_to_int(exponent_length_bytes)
+
+    modulus_length_bytes = pad32r(data[64:96])
+    modulus_length = big_endian_to_int(modulus_length_bytes)
+
+    return base_length, exponent_length, modulus_length
+
+
+def _compute_modexp_gas_fee(data):
+    base_length, exponent_length, modulus_length = _extract_lengths(data)
+
+    first_32_exponent_bytes = zpad_right(
+        data[96 + base_length:96 + base_length + exponent_length],
+        to_size=min(exponent_length, 32),
+    )[:32]
+    adjusted_exponent_length = _compute_adjusted_exponent_length(
+        exponent_length,
+        first_32_exponent_bytes,
+    )
     complexity = _compute_complexity(max(modulus_length, base_length))
 
     gas_fee = (
@@ -52,54 +74,56 @@ def _compute_modexp_gas_fee(exponent_bytes, base_length, modulus_length):
     return gas_fee
 
 
+def _modexp(data):
+    base_length, exponent_length, modulus_length = _extract_lengths(data)
+
+    if base_length == 0:
+        return 0
+    elif modulus_length == 0:
+        return 0
+
+    # compute start:end indexes
+    base_end_idx = 96 + base_length
+    exponent_end_idx = base_end_idx + exponent_length
+    modulus_end_dx = exponent_end_idx + modulus_length
+
+    # extract arguments
+    modulus_bytes = zpad_right(
+        data[exponent_end_idx:modulus_end_dx],
+        to_size=modulus_length,
+    )
+    modulus = big_endian_to_int(modulus_bytes)
+    if modulus == 0:
+        return 0
+
+    base_bytes = zpad_right(data[96:base_end_idx], to_size=base_length)
+    base = big_endian_to_int(base_bytes)
+
+    exponent_bytes = zpad_right(
+        data[base_end_idx:exponent_end_idx],
+        to_size=exponent_length,
+    )
+    exponent = big_endian_to_int(exponent_bytes)
+    print('base', base, 'exponent', exponent, 'modulus', modulus)
+
+    result = pow(base, exponent, modulus)
+
+    return result
+
+
 def precompile_modexp(computation):
     """
     https://github.com/ethereum/EIPs/pull/198
 
     TODO: test against test vectors from EIP
     """
-    # extract argument lengths
-    base_length_bytes = pad32r(computation.msg.data[32:64])
-    base_length = big_endian_to_int(base_length_bytes)
-
-    exponent_length_bytes = pad32r(computation.msg.data[64:96])
-    exponent_length = big_endian_to_int(exponent_length_bytes)
-
-    modulus_length_bytes = pad32r(computation.msg.data[96:128])
-    modulus_length = big_endian_to_int(modulus_length_bytes)
-
-    # extract arguments
-    base_end_idx = 128 + base_length
-    base_bytes = zpad_right(computation.msg.data[128:base_end_idx], to_size=base_length)
-    base = big_endian_to_int(base_bytes)
-
-    exponent_end_idx = base_end_idx + exponent_length
-    exponent_bytes = zpad_right(
-        computation.msg.data[base_end_idx:exponent_end_idx],
-        to_size=exponent_length,
-    )
-    exponent = big_endian_to_int(exponent_bytes)
-
-    modulus_end_dx = exponent_end_idx + modulus_length
-    modulus_bytes = zpad_right(
-        computation.msg.data[exponent_end_idx:modulus_end_dx],
-        to_size=modulus_length,
-    )
-    modulus = big_endian_to_int(modulus_bytes)
-
-    # compute gas cost
-    gas_fee = _compute_modexp_gas_fee(
-        exponent_bytes=exponent_bytes,
-        base_length=base_length,
-        modulus_length=modulus_length,
-    )
+    gas_fee = _compute_modexp_gas_fee(computation.msg.data)
     computation.gas_meter.consume_gas(gas_fee, reason='MODEXP Precompile')
 
-    if modulus == 0:
-        result = 0
-    else:
-        result = pow(base, exponent, modulus)
+    result = _modexp(computation.msg.data)
 
+    _, _, modulus_length = _extract_lengths(computation.msg.data)
     result_bytes = zpad_left(int_to_big_endian(result), to_size=modulus_length)
+
     computation.output = result_bytes
     return computation
