74b47a24c88ae9d31a48622a29b16598ddaf3426
Piper Merriam <pipermerriam@gmail.com>
2017-11-15 16:07:51 -0700

    remove mutation from pairing code

--- evm/precompiles/ecpairing.py
+++ evm/precompiles/ecpairing.py
@@ -1,3 +1,7 @@
+from cytoolz import (
+    curry,
+    pipe,
+)
 from py_ecc import (
     optimized_bn128 as bn128,
 )
@@ -47,33 +51,42 @@ def precompile_ecpairing(computation):
 
 
 def _ecpairing(data):
-    for start_idx in range(0, len(data), 192):
-        x1, y1, x2_i, x2_r, y2_i, y2_r = _extract_point(data[start_idx:start_idx + 192])
-        p1 = validate_point(x1, y1)
+    exponent = bn128.FQ12.one()
 
-        exponent = bn128.FQ12.one()
-        for v in (x2_i, x2_r, y2_i, y2_r):
-            if v >= bn128.field_modulus:
-                raise ValidationError("value greater than field modulus")
-
-        fq2_x = bn128.FQ2([x2_r, x2_i])
-        fq2_y = bn128.FQ2([y2_r, y2_i])
-
-        if (fq2_x, fq2_y) != (bn128.FQ2.zero(), bn128.FQ2.zero()):
-            p2 = (fq2_x, fq2_y, bn128.FQ2.one())
-            if not bn128.is_on_curve(p2, bn128.b2):
-                raise ValidationError("point is not on curve")
-        else:
-            p2 = ZERO
-
-        if bn128.multiply(p2, bn128.curve_order)[-1] != bn128.FQ2.zero():
-            raise ValidationError("TODO: what case is this?????")
-
-        # TODO: remove mutation
-        exponent *= bn128.pairing(p2, p1, final_exponentiate=False)
+    processing_pipeline = (
+        _process_point(data[start_idx:start_idx + 192])
+        for start_idx
+        in range(0, len(data), 192)
+    )
+    exponent = pipe(bn128.FQ12.one(), *processing_pipeline)
 
     result = bn128.final_exponentiate(exponent) == bn128.FQ12.one()
     return result
+
+
+@curry
+def _process_point(data_buffer, exponent):
+    x1, y1, x2_i, x2_r, y2_i, y2_r = _extract_point(data_buffer)
+    p1 = validate_point(x1, y1)
+
+    for v in (x2_i, x2_r, y2_i, y2_r):
+        if v >= bn128.field_modulus:
+            raise ValidationError("value greater than field modulus")
+
+    fq2_x = bn128.FQ2([x2_r, x2_i])
+    fq2_y = bn128.FQ2([y2_r, y2_i])
+
+    if (fq2_x, fq2_y) != (bn128.FQ2.zero(), bn128.FQ2.zero()):
+        p2 = (fq2_x, fq2_y, bn128.FQ2.one())
+        if not bn128.is_on_curve(p2, bn128.b2):
+            raise ValidationError("point is not on curve")
+    else:
+        p2 = ZERO
+
+    if bn128.multiply(p2, bn128.curve_order)[-1] != bn128.FQ2.zero():
+        raise ValidationError("TODO: what case is this?????")
+
+    return exponent * bn128.pairing(p2, p1, final_exponentiate=False)
 
 
 def _extract_point(data_slice):
